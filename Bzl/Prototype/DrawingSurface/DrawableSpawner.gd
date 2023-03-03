df5e02bae4bf92a7f1ccc610a497857212ac906a
Dokkae6949 <finnliry@gmail.com>
2023-03-03 22:15:24 +0100

    Basic spawn, drag and drop system implemented

--- Bzl/Prototype/DrawingSurface/DrawableSpawner.gd
+++ Bzl/Prototype/DrawingSurface/DrawableSpawner.gd
@@ -1,7 +1,15 @@
 extends Node
+class_name DrawableSpawner
 
 
+# every drawable currently spawned
 var drawables := []
+
+# drawables the mouse is hovering over
+var hovered_drawables := []
+
+# drawable which is currently being draged
+var active_drawable
 
 onready var drawable_square = preload("res://Prototype/DrawingSurface/Drawable/DrawableSquare.tscn")
 onready var drawable_circle = preload("res://Prototype/DrawingSurface/Drawable/DrawableCircle.tscn")
@@ -9,32 +17,77 @@ onready var drawable_circle = preload("res://Prototype/DrawingSurface/Drawable/D
 
 func spawn_drawable(drawable):
 	var instance = drawable.instance()
+	instance.disable()
+	instance.drawable_spawner = self
 	drawables.push_back(instance)
-	instance.connect("pressed", self, "on_drawable_pressed")
-	instance.connect("deleted", self, "on_drawable_deleted")
+	_update_z_index(drawables, instance)
 	add_child(instance)
-	
 
-func on_drawable_pressed(origin):
-	var index := drawables.find(origin)
+
+func _input(event):
+	if event.is_action_pressed("left_click"):
+		if active_drawable != null:
+			return
+			
+		active_drawable = _get_last(hovered_drawables)
+		
+		if active_drawable == null:
+			return
+		
+		active_drawable.enable()
+		
+	elif event.is_action_released("left_click"):
+		if active_drawable == null:
+			return
+			
+		active_drawable.disable()
+		active_drawable = null
+
+
+func _update_z_index(array: Array, drawable):
+	drawable.z_index = array.find(drawable)
+
+	
+func _update_z_indecies(array: Array):
+	for i in range(0, array.size()):
+		array[i].z_index = i
+
+
+func _sort_by_z_index(array: Array):
+	array.sort_custom(self, "_sort_by_z_index_ascent")
+	
+	
+func _sort_by_z_index_ascent(a, b):
+	if a.z_index < b.z_index:
+		return true
+	return false
+
+
+func _add_to_hovered(drawable):
+	if hovered_drawables.find(drawable) >= 0:
+		return
+	
+	hovered_drawables.push_back(drawable)
+	_sort_by_z_index(hovered_drawables)
+	
+	print(hovered_drawables)
+
+
+func _remove_from_hovered(drawable):
+	var index = hovered_drawables.find(drawable)
 	
 	if index < 0:
 		return
-	
-	drawables.remove(index)
-	drawables.push_back(origin)
-	
-	for i in range(0, drawables.size()):
-		drawables[i].z_index = i
 
-
-func on_drawable_deleted(origin):
-	var index := drawables.find(origin)
+	hovered_drawables.remove(index)
+	_sort_by_z_index(hovered_drawables)
 	
-	if index < 0:
+	
+func _get_last(array: Array):
+	if array.size() == 0:
 		return
-
-	drawables.remove(index)
+		
+	return array[array.size() - 1]
 
 
 func _on_spawn_square_buttom_pressed():
