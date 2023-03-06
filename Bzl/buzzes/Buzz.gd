53df43e84cc08821054f4668aa4352df91ffff66
Dokkae6949 <finnliry@gmail.com>
2023-03-06 21:47:05 +0100

    Implemented every value

--- Bzl/buzzes/Buzz.gd
+++ Bzl/buzzes/Buzz.gd
@@ -1,15 +1,32 @@
 extends Panel
 
 
-@export var text = ""
-@export var keywords = ["hello", "kitty"]
-@export var remaining_time = 0
-@export var author = ""
+@export var update_on_startup := false
 
+@export var title := ""
+@export var author := ""
+@export var lifetime := 0
+@export var text := ""
+@export var keywords: Array[String] = []
+
+@onready var title_label = $MarginContainer/VBoxContainer/Header/InfoMargins/Info/Title
+@onready var author_label = $MarginContainer/VBoxContainer/Header/InfoMargins/Info/HBoxContainer/Author
+@onready var lifetime_label = $MarginContainer/VBoxContainer/Header/InfoMargins/Info/HBoxContainer/Lifetime
+@onready var text_label = $MarginContainer/VBoxContainer/Text
 @onready var keywords_label = $MarginContainer/VBoxContainer/Keywords
 
 
 func _ready():
+	if update_on_startup:
+		update_values()
+
+
+func update_values():
+	title_label.text = title
+	author_label.text = author
+	lifetime_label.text = str(lifetime)
+	text_label.text = text
+	
 	keywords_label.text = ""
 	
 	for i in range(0, keywords.size()):
