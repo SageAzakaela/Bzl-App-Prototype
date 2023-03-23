eae14454adb30f2e3be3815fd93001517f9d6449
Dokkae6949 <finnliry@gmail.com>
2023-03-23 10:00:58 +0100

    Added post creation validation check

--- Bzl/main/content-pages/CreationContent/CreationContent.gd
+++ Bzl/main/content-pages/CreationContent/CreationContent.gd
@@ -9,6 +9,19 @@ var keywords_input : Array[String]
 
 
 func _on_post_button_pressed():
+	if keywords_input.count("") == keywords_input.size():
+		print("No keywords provided!")
+		
+	for keyword in keywords_input:
+		if keyword.begins_with(" "):
+			print("Invalid keywords provided!")
+		
+	if title_input.text == "":
+		print("No title provided")
+		
+	if text_input.text == "":
+		print("No content provided!")
+	
 	BuzzManager.create_buzz(title_input.text, text_input.text, keywords_input)
 	
 	
